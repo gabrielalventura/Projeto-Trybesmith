1f44e7a4bf672b222a453fff7b67d6017a530e42
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-14 19:12:13 -0300

    Add: Adiciona a rota para users

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import productsRouter from './routes/productsRouter';
+import usersRouter from './routes/userRouter';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', productsRouter);
+app.use('/users', usersRouter);
 
 export default app;
