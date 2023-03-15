908cacba3584c53f390ff3e21616e4cc68963f4a
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-15 15:06:05 -0300

    Implementa rota de login

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import loginRouter from './routes/loginRouter';
 import productsRouter from './routes/productsRouter';
 import usersRouter from './routes/userRouter';
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use('/products', productsRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 
 export default app;
