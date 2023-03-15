efabad4cd05b7cc769e97abae95fcad6224d7c1d
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-15 18:24:34 -0300

    Cria rota para orders

---
+++ src/routes/orderRouter.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import orderController from '../controllers/orderController';
+
+const orderRouter = Router();
+
+orderRouter.get('/', orderController.getAllOrders);
+
+export default orderRouter;
