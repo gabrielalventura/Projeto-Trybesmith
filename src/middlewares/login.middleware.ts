e68f02d0fcd65815803c2565839f756867d90db8
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-15 14:59:44 -0300

    Create: Cria validação para login

---
+++ src/middlewares/login.middleware.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+import { Ilogin } from '../interfaces';
+
+const loginMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { username, password } = req.body as Ilogin;
+
+  if (username === null) {
+    return res.status(400).json({ message: '"username" is required' });
+  }
+
+  if (password === null) {
+    return res.status(400).json({ message: '"password" is required' });
+  }
+  return next();
+};
+
+export default loginMiddleware;
